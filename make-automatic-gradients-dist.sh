b8e7e0676d90015ea46aa0bbb088b257ae67cc01
Toni <toni.giorgino@gmail.com>
2019-04-17 16:34:46 +0200

    add dummy readme

--- make-automatic-gradients-dist.sh
+++ make-automatic-gradients-dist.sh
@@ -4,4 +4,4 @@ outzip=plumed2-automatic-gradients-dist.zip
 # rm $outzip
 # git submodule deinit --all
 # make clean
-git archive -o $outzip --prefix=plumed2-automatic-gradients-PATCH/ HEAD src/curvature* regtest/curvature* README.md
+git archive -o $outzip --prefix=plumed2-automatic-gradients-PATCH/ HEAD src/curvature* regtest/curvature* README.md plumed-nest-readme.dat
